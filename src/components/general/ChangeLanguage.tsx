54d2f7b8b8936042fe384bb2eea7bee70ceea130
moon <ahmedabozaid.dev@gmail.com>
2025-12-21 11:12:24 +0200

    adds base for header

---
+++ src/components/general/ChangeLanguage.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+
+const ChangeLanguage = () => {
+  const { pathname } = useLocation();
+  const isHomePage = pathname === "/";
+  return (
+    <div className="flex items-center justify-center gap-2">
+      <img
+        src={
+          isHomePage ? "layout/language_black.svg" : "layout/language_white.svg"
+        }
+        alt="Change Language"
+      />
+      <span>English</span>
+    </div>
+  );
+};
+
+export default ChangeLanguage;
--- src/components/general/Header.tsx
+++ src/components/general/Header.tsx
@@ -1,9 +1,35 @@
-const Header = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+import { NavLinks } from "@/constants/NavLinks";
+import { Link, useLocation } from "react-router-dom";
+import ChangeLanguage from "./ChangeLanguage";
 
-export default Header
+const Header = () => {
+  const { pathname } = useLocation();
+  const isHomePage = pathname === "/";
+  return (
+    <header className="container flex justify-between fixed top-0 left-0 right-0 z-50 ">
+      <Link to="/">
+        <img
+          src={isHomePage ? "/logo_blue.svg" : "/logo_white.svg"}
+          alt="Logo"
+        />
+      </Link>
+      <div>
+        {NavLinks.map((link) => (
+          <Link
+            key={link.path}
+            to={link.path}
+            className={`mx-4 ${isHomePage ? "text-blue-600" : "text-white"}`}
+          >
+            {link.nameEn}
+          </Link>
+        ))}
+      </div>
+      <div className="flex items-center gap-6">
+        <ChangeLanguage />
+        <span>Request a demo</span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
